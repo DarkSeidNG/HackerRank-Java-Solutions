c7a797f07428da882688b12cb86d27035ce3aaa7
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-01-16 16:02:45 +0100

    Added first java question file and solution

---
+++ src/welcome_to_java/Solution.java
@@ -0,0 +1,17 @@
+package welcome_to_java;
+
+/**
+ * The type Solution.
+ */
+public class Solution {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
