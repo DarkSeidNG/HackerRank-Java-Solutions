a770fc3cfa7ecbe46335f394b1d82335de2d2887
Ifeanyi Orji <chrisifwax101@gmail.com>
2019-01-16 16:07:34 +0100

    Added Java Stdin and Stdout I question file and solution

---
+++ src/java_stdin_and_stdout_1/Solution.java
@@ -0,0 +1,25 @@
+package java_stdin_and_stdout_1;
+
+import java.util.*;
+
+/**
+ * The type Solution.
+ */
+public class Solution {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
